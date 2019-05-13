8bebce95cecc397b894d2c8449b46cfe262e1848
Kirill Medvedev <Medvedev-kv@list.ru>
2019-05-13 18:05:14 +0300

    Initial Commit

---
+++ LifeCycleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycleApp
+//
+//  Created by Кирилл Медведев on 13/05/2019.
+//  Copyright © 2019 Kirill Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
